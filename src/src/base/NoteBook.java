3971757edcfd9d9079263de91e571c8b8c5853b4
mwsmartin <adsou0112233@gmail.com>
2016-10-04 22:20:56 +0800

    lab5

--- src/src/base/NoteBook.java
+++ src/src/base/NoteBook.java
@@ -1,9 +1,15 @@
 package base;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NoteBook {
+public class NoteBook implements  Serializable{
+	private static final long serialVersionUID = 1L;
 	private ArrayList<Folder> folders;
 	public NoteBook(){
 		folders = new ArrayList<Folder>();
@@ -12,6 +18,10 @@ public class NoteBook {
 		TextNote note = new TextNote(title);
 		return insertNote(folderName,note);
 	}
+	public boolean createTextNote(String folderName, String title, String content){
+		TextNote note = new TextNote(title, content);
+		return insertNote(folderName, note);
+	}
 	public boolean createImageNote(String folderName, String title){
 		ImageNote note = new ImageNote(title);
 		return insertNote(folderName,note);
@@ -19,15 +29,54 @@ public class NoteBook {
 	public ArrayList<Folder> getFolders(){
 		return folders;
 	}
+	
+	public NoteBook(String file){
+		FileInputStream fis = null;
+		ObjectInputStream in = null;
+		try {
+			fis = new FileInputStream(file);
+			in = new ObjectInputStream(fis);
+			NoteBook n = (NoteBook) in.readObject();
+			in.close();
+			} catch (Exception e) {
+			e.printStackTrace();
+			}
+		}
+	
+	public boolean save(String file){
+		//TODO
+		FileOutputStream fos = null;
+		ObjectOutputStream out = null;
+		NoteBook note = new NoteBook();
+		note = this;
+		try {
+		//TODO
+			fos = new FileOutputStream(file);
+			out = new ObjectOutputStream(fos);
+			out.writeObject(note);
+			out.close();		
+			
+		} catch (Exception e) {
+		 return false;
+		}
+		return true;
+		}
+	
+	
+	
+    public int compareTo(Note o){
+    	//String ME =  date.toString();
+    	//String Other = o.date.toString();
+    	//int result = ME.compareTo(Other);
+    	//return result;
+    	return 0;
+    	
+    }
 	public void sortFolders(){
 		for (Folder f1:folders){
 			f1.sortNotes();
 		}
 		
-	}
-	public boolean createTextNote(String folderName, String title, String content){
-		TextNote note = new TextNote(title, content);
-		return insertNote(folderName, note);
 	}
 	private boolean insertNote(String folderName,Note note){
 		Folder f = null;
--- src/src/base/TextNote.java
+++ src/src/base/TextNote.java
@@ -1,5 +1,13 @@
 package base;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class TextNote extends Note {
 	public String content;
 	public TextNote(String title){
@@ -10,6 +18,53 @@ public class TextNote extends Note {
 		this.content=content;
 		
 	}
+	public TextNote(File f) {
+		super(f.getName());
+		this.content = getTextFromFile(f.getAbsolutePath());
+		}
+		/**
+		* get the content of a file
+		*
+		* @param absolutePath of the file
+		* @return the content of the file
+		*/
+		private String getTextFromFile(String absolutePath) {
+		String result = "";
+		// TODO
+		BufferedReader br;
+        try {
+            br = new BufferedReader(new FileReader(absolutePath));
+            try {
+                String x = br.readLine();
+                while ( x != null ) {
+                    // printing out each line in the file
+                    result += x;
+                } 
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+		return result;
+		}
+		public void exportTextToFile(String pathFolder) {
+			String text= this.content;
+			BufferedWriter bw;
+			File file = new File( pathFolder + File.separator + this.getTitle() + ".txt");
+			try {
+				file.createNewFile();
+				 bw = new BufferedWriter(new FileWriter(file));
+				 bw.write(text);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			// TODO
+			}
 	/*public boolean createTextNote(String folderName, String title, String content){
 		TextNote note = new TextNote(title, content);
 		return insertNote(folderName, note);
